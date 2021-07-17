bd53c98faab22c22c5d14041a4974f0a5130cf01
qq240814476 <240814476@qq.com>
2021-07-18 00:10:19 +0800

    feat: Ex. equal to exercise

--- src/py/01-day1-modalwhale.py
+++ src/py/01-day1-modalwhale.py
@@ -1,14 +1,17 @@
-# Exercise / Practice one
+# Exercise one
+# Ex.1
 print('Question 1: ')
 print('hello world !*', end='')
 print('123456', sep='')
 
 # Exercise / Practice two
+# Ex.2
 print('Question 2: ')
 f = int(input('Input Fahrenheit number, pls : \n'))
 print((f-32) / 1.8)
 
 # Exercise / Practice three
+# Ex.3
 print('Question 3: ')
 radis = int(input('Input the radis of circle, pls: \n'))
 print("Radis: " + str(radis) + " Square: " + str(radis ** 2 * 3.14) + " Perimeter: " + str(2 * 3.14 * radis))
