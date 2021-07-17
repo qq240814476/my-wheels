4bddb4b9740388358a492dab5e31c433c88792dc
qq240814476 <240814476@qq.com>
2021-07-18 00:04:45 +0800

    feat: hs & py

---
+++ src/haskell/hello-haskell.hs
@@ -0,0 +1,5 @@
+doubleMe x = x + x
+doubleUs x y = x * 2 + y * 2
+let numbers = [0,1,2,3]
+4: numbers
+numbers
---
+++ src/py/01-day1-modalwhale.py
@@ -0,0 +1,14 @@
+# Exercise / Practice one
+print('Question 1: ')
+print('hello world !*', end='')
+print('123456', sep='')
+
+# Exercise / Practice two
+print('Question 2: ')
+f = int(input('Input Fahrenheit number, pls : \n'))
+print((f-32) / 1.8)
+
+# Exercise / Practice three
+print('Question 3: ')
+radis = int(input('Input the radis of circle, pls: \n'))
+print("Radis: " + str(radis) + " Square: " + str(radis ** 2 * 3.14) + " Perimeter: " + str(2 * 3.14 * radis))
